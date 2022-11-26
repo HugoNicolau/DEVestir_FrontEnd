25f032f53302e5c5be5a1b78b085ebd5af50da55
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-11-25 23:15:01 -0300

    Preview mainScreen

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,42 @@
-
+import styled from "styled-components";
 
 export default function Shop(){
 
-    return("Hello World");
+    return(
+        <ScreenContainer>
+            <MainContainer>
+
+            </MainContainer>
+            <CartContainer>
+
+            </CartContainer>
+        </ScreenContainer>
+    );
 }
+
+const ScreenContainer = styled.div`
+width:100vw;
+padding-top:90px;
+height:100vh;
+background-color:rebeccapurple;
+display:flex;
+align-items:center;
+justify-content:center;
+`
+const MainContainer = styled.div`
+width:80vw;
+height:100vh;
+background-color:rebeccapurple;
+display:flex;
+align-items:center;
+justify-content:center;
+`
+
+const CartContainer = styled.div`
+width:20vw;
+height:100vh;
+background-color:whitesmoke;
+display:flex;
+align-items:center;
+justify-content:center;
+`
