9fe6ec6fac934a62923efe9e92260d09ee03c554
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-11-28 12:39:45 -0300

    Fitting images with different widths and heights

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -256,7 +256,7 @@ box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 img{
     width:100%;
     height:77%;
-    /* object-fit: cover; */
+    object-fit: cover;
     border-radius:8px 8px 0 0;
     
    
